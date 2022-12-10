e8598ab26a574bab9d7ffa7b0bb619310ec9ce07
CHA-SOYOUNG <chdo5412@naver.com>
2022-12-10 18:51:18 +0900

    zz

--- app/src/main/java/com/example/a1209_app/Result.java
+++ app/src/main/java/com/example/a1209_app/Result.java
@@ -1,2 +1,26 @@
-package com.example.a1209_app;public class Result {
+package com.example.a1209_app;
+
+public class Result {
+    public static String result;
+
+    private static final Result instance = new Result();
+
+    private Result() {};
+    public static Result Getinstance()
+    {
+        return instance;
+    }
+
+
+    public Result (String result){
+        this.result = result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
 }
